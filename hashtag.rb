06c5ed70012611d5ff34b82f758e7d24a052f633
Ben P <ben.h.poh@gmail.com>
2020-07-17 11:58:53 +1000

    hashtag exercise completed

---
+++ hashtag.rb
@@ -0,0 +1,36 @@
+# The marketing team is spending way too much time typing in hashtags.
+# Let's help them with our own Hashtag Generator!
+
+# Here's the deal:
+
+# It must start with a hashtag (#).
+# All words must have their first letter capitalized.
+# If the final result is longer than 140 chars it must return false.
+# If the input or the result is an empty string it must return false.
+# Examples
+# " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+# "    Hello     World   "                  =>  "#HelloWorld"
+# ""                                        =>  false
+
+require "pry"
+
+def generateHashtag(str)
+  words = str.split(" ")
+  return false unless words[0]
+  hash_str = []
+
+  words.each do |word|
+    hash_str << word.capitalize
+  end
+
+  hashtag = "#" + hash_str.join("")
+
+  if hashtag.length > 140
+    return false
+  else
+    hashtag
+  end
+  
+end
+
+binding.pry
